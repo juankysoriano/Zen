2ae9c7a2ead62f504426718d166194419ca5220f
Juanky Soriano <juanky@juankysoriano.com>
2015-02-23 23:43:35 +0000

    Refactors library location

--- rainbow-lib/src/main/java/com/juankysoriano/rainbow/core/cv/blobdetector/OnBlobDetectedCallback.java
+++
@@ -1,7 +0,0 @@
-package com.juankysoriano.rainbow.core.cv.blobdetector;
-
-public interface OnBlobDetectedCallback {
-    void onBlobDetected(Blob b);
-    boolean isToDiscardBlob(Blob b);
-    void onBlobDetectionFinish();
-}
--- rainbow-lib/src/main/java/com/juankysoriano/rainbow/core/exeption/RainbowLifeCycleException.java
+++
@@ -1,8 +0,0 @@
-package com.juankysoriano.rainbow.core.exeption;
-
-public class RainbowLifeCycleException extends RuntimeException {
-
-    public RainbowLifeCycleException(String error) {
-        super(error);
-    }
-}
