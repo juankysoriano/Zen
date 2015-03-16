07f63fa320038d429b81360b85507312a27b31e1
Juanky Soriano <juanky@juankysoriano.com>
2015-03-16 22:23:03 +0000

    Introduces media player thing

--- zen-android/src/main/java/zenproject/meditation/android/sketch/performers/MusicPerformer.java
+++ zen-android/src/main/java/zenproject/meditation/android/sketch/performers/MusicPerformer.java
@@ -1,25 +1,84 @@
 package zenproject.meditation.android.sketch.performers;
 
+import android.media.MediaPlayer;
+
+import com.juankysoriano.rainbow.core.event.RainbowInputController;
+
+import zenproject.meditation.android.ContextRetriever;
+import zenproject.meditation.android.R;
+
 public class MusicPerformer implements StepPerformer {
-    private boolean enabled;
+    private final MediaPlayer mediaPlayer;
+    private final RainbowInputController rainbowInputController;
+    private float volume;
+
+    protected MusicPerformer(MediaPlayer mediaPlayer, RainbowInputController rainbowInputController) {
+        this.mediaPlayer = mediaPlayer;
+        this.rainbowInputController = rainbowInputController;
+    }
+
+    public static MusicPerformer newInstance(RainbowInputController rainbowInputController) {
+        MediaPlayer mediaPlayer = MediaPlayer.create(ContextRetriever.INSTANCE.getCurrentContext(), R.raw.zen);
+        mediaPlayer.setVolume(0, 0);
+        mediaPlayer.start();
+        return new MusicPerformer(mediaPlayer, rainbowInputController);
+    }
 
     @Override
     public void doStep() {
-        //no-op
+        if (rainbowInputController.isScreenTouched()) {
+            increaseVolume();
+        } else {
+            decreaseVolume();
+        }
+    }
+
+    private void increaseVolume() {
+        if (mediaPlayer.isPlaying()) {
+            volume += 0.02f;
+            volume = Math.min(1f, volume);
+            mediaPlayer.setVolume(volume, volume);
+        }
+    }
+
+    private void decreaseVolume() {
+        if (mediaPlayer.isPlaying()) {
+            volume -= 0.02f;
+            volume = Math.max(0, volume);
+            mediaPlayer.setVolume(volume, volume);
+        }
     }
 
     @Override
     public void reset() {
-        //no-op
+        volume = 0f;
+        mediaPlayer.setVolume(volume, volume);
     }
 
     @Override
     public void disable() {
-        enabled = false;
+        stopMusic();
+    }
+
+    private void stopMusic() {
+        if (mediaPlayer.isPlaying()) {
+            mediaPlayer.pause();
+        }
     }
 
     @Override
     public void enable() {
-        enabled = true;
+        reset();
+        startMusic();
+    }
+
+    private void startMusic() {
+        if (!mediaPlayer.isPlaying()) {
+            mediaPlayer.start();
+        }
+    }
+
+    public void release() {
+        mediaPlayer.release();
     }
 }
