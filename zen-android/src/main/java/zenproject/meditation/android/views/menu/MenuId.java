541aedaaa7700c58788116e75879c62abf987e4d
Juanky Soriano <juanky@juankysoriano.com>
2015-04-05 00:31:28 +0100

    Makes values a constant

--- zen-android/src/main/java/zenproject/meditation/android/views/menu/MenuId.java
+++ zen-android/src/main/java/zenproject/meditation/android/views/menu/MenuId.java
@@ -4,17 +4,17 @@ import android.support.annotation.IdRes;
 
 public abstract class MenuId {
     @IdRes
-    public static int BRUSH_ID = 1;
+    public static final int BRUSH_ID = 1;
     @IdRes
-    public static int RESTART_ID = 2;
+    public static final int RESTART_ID = 2;
     @IdRes
-    public static int MUSIC_ID = 3;
+    public static final int MUSIC_ID = 3;
     @IdRes
-    public static int FLOWERS_ID = 4;
+    public static final int FLOWERS_ID = 4;
     @IdRes
-    public static int SAVE_ID = 5;
+    public static final int SAVE_ID = 5;
     @IdRes
-    public static int SHARE_ID = 6;
+    public static final int SHARE_ID = 6;
     @IdRes
-    public static int MENU_ID = 7;
+    public static final int MENU_ID = 7;
 }
